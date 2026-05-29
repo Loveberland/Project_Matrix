8f755a3e5897d7e15adfa7bbc5abd82e719776a9
Loveberland <lovekittipong12@gmail.com>
2026-05-29 08:07:43 +0700

    remove out

--- out/display.d
+++
@@ -1,3 +0,0 @@
-out/display.o: src/display.c src/display.h src/matrix.h
-src/display.h:
-src/matrix.h:
--- out/main.d
+++
@@ -1,6 +0,0 @@
-out/main.o: src/main.c src/input_handler.h src/matrix.h src/calculate.h \
- src/display.h
-src/input_handler.h:
-src/matrix.h:
-src/calculate.h:
-src/display.h:
