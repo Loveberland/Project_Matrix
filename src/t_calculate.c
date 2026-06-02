7aa4c8bd0bfa29ed6d3ae755c51a889641511ea1
Loveberland <lovekittipong12@gmail.com>
2026-06-02 09:31:14 +0700

    creat test for mul_mat

--- src/t_calculate.c
+++ src/t_calculate.c
@@ -84,8 +84,57 @@ int t_sub_mat() {
 	return status;
 }
 
+int t_mul_mat() {
+	int status = 0;
+	puts("\n\033[0;33mtest sub_mat function\033[0m");
+	puts("matrix count is 3");
+	puts("matrix size is 2x2");
+	Matrix *mat = malloc(3 * sizeof(Matrix));
+	for (int i = 0; i < 3; i++) {
+		mat[i].data = malloc(ROWS * COLS * sizeof(double));
+		mat[i].rows = ROWS;
+		mat[i].cols = COLS;
+	}
+	for (int i = 0; i < 4; i++)
+		mat[0].data[i] = i + 1;
+	print_mat(&mat[0], "matrix 1:\n");
+
+	for (int i = 0; i < 4; i++)
+		mat[1].data[i] = 1;
+	print_mat(&mat[1], "matrix 2:\n");
+
+	for (int i = 0; i < 4; i++)
+		mat[2].data[i] = (i + 1) * 2;
+	print_mat(&mat[2], "matrix 3:\n");
+
+	status = mul_mat(mat, 3);	
+	if (mat[0].data[0] != 24)
+		status = 1;
+	if (mat[0].data[1] != 36)
+		status = 1;
+	if (mat[0].data[2] != 56)
+		status = 1;
+	if (mat[0].data[3] != 84)
+		status = 1;
+
+	if (status)
+		puts("wrong answer");
+	return status;
+}
+
 int main(void) {
-	t_add_mat();
-	t_sub_mat();
-	return (0);
+	int status = 0;
+	status = t_add_mat();
+	if (status)
+		return status;
+
+	status = t_sub_mat();
+	if (status)
+		return status;
+
+	status = t_mul_mat();
+	if (status)
+		return status;
+
+	return status;
 }
